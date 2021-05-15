3d9167b7b85589a9c26b319fb948d558963a433a
fraromesc <fraromesc@gmail.com>
2021-05-15 10:38:11 +0200

    fil 2 encarna

---
+++ motorVelocidadVariable.py
@@ -0,0 +1,45 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+
+MotorIN1 = 15
+MotorIN2 = 14
+MotorE1 = 18
+
+GPIO.setup(MotorIN1,GPIO.OUT)
+GPIO.setup(MotorIN2,GPIO.OUT)
+GPIO.setup(MotorE1,GPIO.OUT)
+
+p = GPIO.PWM(MotorE1, 50)  # Creamos la instancia PWM con el GPIO a utilizar y la frecuencia de la señal PWM
+p.start(0)  #Inicializamos el objeto PWM
+
+print("Hacemos girar el motor en un sentido por 10 segundos mientras aumentamos  y disminuimos la velocidad")
+GPIO.output(MotorIN1,GPIO.HIGH) # Establecemos el sentido de giro con los pines IN1 e IN2  
+GPIO.output(MotorIN2,GPIO.LOW)  # Establecemos el sentido de giro con los pines IN1 e IN2
+
+for dc in range(0, 101, 1):
+    p.ChangeDutyCycle(dc)
+    time.sleep(0.05)
+
+for dc in range(100, -1, -1):
+    p.ChangeDutyCycle(dc)
+    time.sleep(0.05)
+
+print("Hacemos girar el motor en el sentido contrario por 10 segundos mientras aumentamos  y disminuimos la velocidad")
+GPIO.output(MotorIN1,GPIO.LOW) # Establecemos el sentido de giro con los pines IN1 e IN2  
+GPIO.output(MotorIN2,GPIO.HIGH)  # Establecemos el sentido de giro con los pines IN1 e IN2
+
+for dc in range(0, 101, 1):
+    p.ChangeDutyCycle(dc)
+    time.sleep(0.05)
+
+for dc in range(100, -1, -1):
+    p.ChangeDutyCycle(dc)
+    time.sleep(0.05)
+
+print ("Detenemos el motor")
+p.ChangeDutyCycle(0)
+sleep(1)
+
+GPIO.cleanup()
